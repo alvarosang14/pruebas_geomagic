ae81ab46d2ad1b499e05e52c70a4ae4be9bc7ceb
asantos <alvarosang2003@gmail.com>
2026-02-08 22:47:19 +0100

    [ADD] Added cartesian_to_haptic.py

--- ros2_interfaces/src/geomagic_control_py/geomagic_control_py/cartesian_to_haptic.py
+++ ros2_interfaces/src/geomagic_control_py/geomagic_control_py/cartesian_to_haptic.py
@@ -2,34 +2,70 @@ import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from std_srvs.srv import SetBool
+from std_msgs.msg import Wrench
 
+NODE = 'cartesian_to_haptic'
+HAPTIC_YARP_DEVICE_NODE_NAME = 'haptic_yarp_device'
 
-class MinimalPublisher(Node):
+class CartesianToHaptic(Node):
 
     def __init__(self):
-        super().__init__('cartesian_to_haptic')
-        publishCartesianCreate()
+        super().__init__(NODE)
+        self.publish_cartesian_create()
+        if not self.set_feedback_mode():
+            self.get_logger().error("Failed to configure cartesian control server")
+            return
+        self.haptic_subscriber_create()
+        self.get_logger().info('Cartesian to Haptic Feedback Node started')
+
+    # -------------------------------------------------------------------------------------------
+    def publish_cartesian_create(self):
+        self.m_haptic_feedback_pub = self.create_publisher(String, HAPTIC_YARP_DEVICE_NODE_NAME + '/feedback', 10)
+
+    def set_feedback_mode(self):
+        self.client_set_feedback_mode = self.create_client(SetBool, 'set_feedback_mode')
         
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        request = SetBool.Request()
+        request.data = False
+        
+        while not self.client_set_feedback_mode.wait_for_service(timeout_sec=1.0):
+            if not rclpy.ok():
+                self.get_logger().error('Interrupted while waiting for the service. Exiting.')
+                return False
+            self.get_logger().info('service not available, waiting again...')
+        
+        future = self.client_set_feedback_mode.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        
+        if future.result() is not None:
+            self.get_logger().info('Feedback mode set successfully')
+            return future.result().success
+        else:
+            self.get_logger().error('Failed to call service set_feedback_mode')
+            return False
+    
+    def haptic_subscriber_create(self):
+        self.create_subscription(
+            Wrench,
+            '/state',
+            self.haptic_state_callback,
+            10
+        )
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    # -------------------------------------------------------------------------------------------
+    def haptic_state_callback(self, msg):
+        self.publish_haptic_feedback(msg)
 
+    # -------------------------------------------------------------------------------------------
+    def publish_haptic_feedback(self, feedback):
+        self.m_haptic_feedback_pub.publish(feedback)
+        self.get_logger().info(f"Published haptic feedback: {feedback.force.x}, {feedback.force.y}, {feedback.force.z}")
 
 def main(args=None):
     try:
         with rclpy.init(args=args):
-            minimal_publisher = MinimalPublisher()
-
-            rclpy.spin(minimal_publisher)
+            rclpy.spin(CartesianToHaptic())
     except (KeyboardInterrupt, ExternalShutdownException):
         pass
 
